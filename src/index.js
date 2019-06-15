09ac3a7382a0fe25b7c6c426ec86e7717f380012
Rafael Brandino <rafael.brandino@softplan.com.br>
2019-06-15 16:27:08 -0300

    Install Jest test lib

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const winston = require('winston')
+const express = require('./config/express')
+
+const app = express.init()
+const port = process.env.PORT || 3000
+
+global.logger = winston.createLogger({
+  transports: [new winston.transports.Console()]
+})
+
+app.listen(port, () => {
+  global.logger.info('Server Listening', { port })
+})
